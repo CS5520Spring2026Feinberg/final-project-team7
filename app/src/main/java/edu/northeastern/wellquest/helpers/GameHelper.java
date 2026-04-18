635ea5c187fabd4a51bf2cadbb30646a020e192e
saravjitsingh <singhsaravjit48@gmail.com>
2026-04-18 18:06:23 -0400

    Update: Raid Boss defeat logic and Fix: the Guild model.

--- app/src/main/java/edu/northeastern/wellquest/helpers/GameHelper.java
+++ app/src/main/java/edu/northeastern/wellquest/helpers/GameHelper.java
@@ -2,6 +2,7 @@ package edu.northeastern.wellquest.helpers;
 
 import android.util.Log;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
